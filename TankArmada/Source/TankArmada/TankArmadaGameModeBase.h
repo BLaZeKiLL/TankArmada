76843ee68727d1afe1983d30e3b2fe7935bc7629
BLaZeKiLL <laldevashish@gmail.com>
2018-01-06 01:11:43 +0530

    INITIAL COMMIT
    Signed-off-by: BLaZeKiLL <laldevashish@gmail.com>

---
+++ TankArmada/Source/TankArmada/TankArmadaGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankArmadaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKARMADA_API ATankArmadaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
