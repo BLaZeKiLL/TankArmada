1f30c95e246d2429133469efe54732607e4d3993
BLaZeKiLL <laldevashish@gmail.com>
2018-01-20 01:56:46 +0530

    Re-Created Class TankPlayerController

---
+++ TankArmada/Source/TankArmada/Public/TankPlayerController.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+//ADD INCLUDE"S HERE
+
+
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKARMADA_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
